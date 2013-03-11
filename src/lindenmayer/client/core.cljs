99d7d4b9f2ed6cb422d0f47072e7cf821b1fb61e
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-11 22:11:28 +0000

    Removed unused code

--- src/lindenmayer/client/core.cljs
+++ src/lindenmayer/client/core.cljs
@@ -4,66 +4,6 @@
         [monet.canvas :only [get-context]]
         [jayq.core :only [$ document-ready attr]]))
 
-; ==============================================
-; Dragon
-;   start  : FX
-;   rules  : (X → X+YF), (Y → FX-Y)
-; ==============================================
-
-(def dragon-seq
-  "Unfolding the dragon"
-  (letfn [(seq0 [x y] 
-            (lazy-seq (cons (flatten x) (seq0 [x :right y :fwd] [:fwd x :left y]))))]
-    (seq0 [] []))) 
-
-; ==============================================
-; Koch Curve
-;   start  : F
-;   rules  : (F → F+F−F−F+F)
-; ==============================================
-
-(def koch-curve-seq
-  (letfn [(seq0 [f]
-            (lazy-seq (cons (flatten f) (seq0 [f :right f :left f :left f :right f]))))]
-    (seq0 [:fwd])))
-
-; ==============================================
-; Sierpinski Triangle
-;   start  : A
-;   rules  : (A → B−A−B), (B → A+B+A)
-;   angle  : 60°
-; ==============================================
-
-(def sierpinski-triangle-seq
-  (letfn [(seq0 [f g] 
-            (lazy-seq (cons (flatten f) (seq0 
-                                          [f :left g :right f :right g :left f]   
-                                          [g g]))))]
-   (seq0 [:fwd] [:fwd]))) 
-
-
-(def sierpinski-curve-seq
-  (letfn [(seq0 [a b] 
-            (lazy-seq (cons (flatten a) (seq0 
-                                          [b :left a :left b] 
-                                          [a :right b :right a]))))]
-    (seq0 [:fwd] [:fwd]))) 
-
-
-(def sierpinski-median-curve-seq
-  (letfn [(seq0 [l r] 
-            (lazy-seq (cons (flatten l) (seq0 
-                                          [:right r :left :fwd :left r :right] 
-                                          [:left l :right :fwd :right l :left]))))]
-    (seq0 [] []))) 
-
-(def space-filling-curve-seq
-  (letfn [(seq0 [x y]
-            (lazy-seq (cons (flatten x) (seq0 
-                                          [:left y :fwd :right x :fwd x :right :fwd y :left] 
-                                          [:right x :fwd :left y :fwd y :left :fwd x :right]))))]
-    (seq0 [] [])))
-
 (defn available-area []
   (let [div (first ($ :div#wrapper))]
     [ (.-offsetWidth div) (.-offsetHeight div) ]))
