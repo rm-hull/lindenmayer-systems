0f0b14478493b487290d808e7b069adbce6f3946
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-13 00:00:36 +0000

    Refactored into a more generalized turtle

--- src/lindenmayer/client/turtle.cljs
+++ src/lindenmayer/client/turtle.cljs
@@ -2,7 +2,17 @@
   (:use [monet.canvas :only [save restore stroke-width stroke-cap stroke-style 
                              begin-path line-to stroke close-path transform]]))
 
-(def colors [:red :green :blue :yellow :cyan :magenta :black "#663300" "#68FF33"])
+;; Basic Turtle implementation
+;; ---------------------------
+;; Allows a seq of keywords to 'drive' a turtle on an HTML5 canvas:
+;;
+;;   (def commands '(:color :red, :fwd 50, :left 10, :fwd 45, :right 72)
+;;   (draw! ctx commands [100 100])
+;;
+;; By default, the drawing is scaled to fill the screen dimensions
+;; (in this case [100,100])
+
+(def colors [:red :green :blue :yellow :cyan :magenta :black :white "#663300" "#68FF33"])
 
 (defn- bounding-box 
   "Calculates the smallest and largest [x,y] points"
@@ -16,30 +26,42 @@
 (defn- deg->rad [theta]
   (* theta radians))
 
-(defn- update-position [state cmd]
-  (if (= cmd :fwd)
-    (let [rad   (deg->rad (:heading state))
-          dist  (:distance state) 
-          [x y] (:coords state)]
-      (assoc state :coords [ (+ x (* dist (Math/cos rad))) 
-                             (+ y (* dist (Math/sin rad)))]))
-    state))
+(defn- move-forward 
+  "Given a state (containing a heading), move forward by the supplied
+   distance."
+  [state dist]
+  (let [rad   (deg->rad (:heading state))
+        [x y] (:coords state)]
+    (assoc state :coords [ (+ x (* dist (Math/cos rad))) 
+                           (+ y (* dist (Math/sin rad)))])))
 
-(defn- update-heading [state cmd]
-  (let [heading (:heading state)
-        turn    (case cmd
-                  :left  (- (:angle state))
-                  :right (:angle state)
-                  0)]
-    (assoc state :heading (+ heading turn))))
+(defn- turn 
+  "Given a state, and an operation (either the + or - function),
+   update such that the new heading is altered by the angle"
+  [op state angle]
+  (let [heading (:heading state)]
+    (assoc state :heading (mod (op heading angle) 360))))
 
-(defn- update-color [state cmd & peek-ahead]
-  (if (and (= cmd :color) (number? (first peek-ahead)))
-    (assoc state :color-index (first peek-ahead))
-    (dissoc state :color-index)))
+(defn- update-color [state color]
+  (assoc state :color color))
 
-(defn- update-stack [state cmd]
+(defn- color-index [index]
+  (get colors index))
+
+(defn- push-state [state _]
   state) ; TODO
+
+(defn- pop-state [state _]
+  state) ; TODO
+
+(def state-mapper
+  { :color   update-color 
+    :color-index (comp update-color color-index) 
+    :left    (partial turn -)
+    :right   (partial turn +)
+    :fwd     move-forward 
+    :save    push-state
+    :restore pop-state })
 
 (defn- next-state 
   "Evolves the current state and a given command to determine the next state,
@@ -48,68 +70,50 @@
   ([] [])
   ([curr-state] curr-state)
   ([curr-state [cmd & peek-ahead]]
-   (if (keyword? cmd)
-     (->
-       curr-state
-       (update-position cmd) 
-       (update-heading cmd) 
-       (update-color cmd peek-ahead)
-       (update-stack cmd)) 
-     curr-state)))
+    (if-let [update-fn (get state-mapper cmd nil)]
+      ; always need stack/heading/coords, but not color
+      (update-fn (dissoc curr-state :color) (first peek-ahead))
+      curr-state)))
 
-(defn- process [color distance angle]
-  (let [init-state { :coords [0 0] :heading 0 :color-index color :stack [] :distance distance :angle angle}]
-    (fn [cmds]
-      (->>
-        cmds
-        (partition-all 2 1)
-        (reductions next-state init-state)))))
+(defn- process [cmds]
+  (let [init-state { :coords [0 0] :heading 0 :stack []}]
+    (->>
+      cmds
+      (partition-all 2 1)
+      (reductions next-state init-state))))
 
-(defn- calc-matrix-transform [[screen-x screen-y] [[min-x min-y] [max-x max-y]]]
+(defn- calc-matrix-transform 
+  "Calculates an affine transform matrix which will scale a drawing 
+   constrained by the min/max bounds to the given screen co-ords. Note
+   that the drawing is flipped so (0,0) will be represented at (or near)
+   the lower edge, not the upper edge."
+  [[screen-x screen-y] [[min-x min-y] [max-x max-y]]]
   (let [scale-x (/ screen-x (- max-x min-x))
         scale-y (/ screen-y (- max-y min-y))
         scale   (min scale-x scale-y)]
-    [ scale 0 0 (- scale) (- min-x) (* scale max-y) ]  
-    )) 
+    [ scale 0 0 (- scale) (- min-x) (* scale max-y) ])) 
 
 (defn- draw-path-segments! [ctx data]
   (doseq [d data]
-    (when-let [index (:color-index d)]
-      (stroke-style ctx (get colors index)))
+    (when-let [color (:color d)]
+      (stroke-style ctx color))
     (apply line-to ctx (:coords d)))
   ctx) ; return the context for threading
 
-(defn draw! [ctx turn-angle cmds available-area]
-  (let [distance 10
-        width    2.5
-        color    0 ;(rand-int (count colors))
-        data   ((process color distance turn-angle) cmds)
+(defn draw! [ctx cmds screen-area]
+  (let [width    2.5
+        data   (process (concat [:color :red] cmds))
         bounds (bounding-box (map :coords data))
-        matrix (calc-matrix-transform available-area bounds)]
-    (.log js/console (pr-str "available-area" available-area))
+        matrix (calc-matrix-transform screen-area bounds)]
+    (.log js/console (pr-str "screen-area" screen-area))
     (.log js/console (pr-str "bounds" bounds))
     (.log js/console (pr-str "matrix" matrix))
+    ;(.log js/console (pr-str "data" data))
     (->
       (apply transform ctx matrix)
       (stroke-width width)
       (stroke-cap :square)
-      (stroke-style :red)
       (begin-path)
       (draw-path-segments! data)
       (stroke)
       (close-path))))
-
-
-;(def s (nth sierpinski-seq 8))
-;
-;(def d (nth dragon-seq 13))
-;
-;((process 0 10 60) d)
-;
-;(def bbox (bounding-box (map :coords ((process 0 10 60) s)))) 
-;
-;bbox
-;
-;(calc-matrix-transform [1000 500] bbox)
-;
-;(* 60 12.5)
