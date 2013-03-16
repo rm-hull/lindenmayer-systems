ea1074f80804b42c707ce356310cdad6c54d38be
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-16 19:32:25 +0000

    Dependent versions

--- project.clj
+++ project.clj
@@ -2,9 +2,10 @@
   :url "http://lindenmayer-systems.destructuring-bind.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [noir "1.3.0"]
                  [jayq "2.1.0"]
+                 [ns-tracker "0.2.1"]
                  [rm-hull/ring-gzip-middleware "0.1.5-SNAPSHOT"]
                  [rm-hull/monet "0.1.6-SNAPSHOT"]]
   :cljsbuild
@@ -17,7 +18,7 @@
         :optimizations :advanced,
         :print-input-delimiter true}}]}
   :hooks [leiningen.dalap leiningen.cljsbuild]
-  :plugins [[lein-cljsbuild "0.2.9"]
+  :plugins [[rm-hull/lein-cljsbuild "0.3.1-SNAPSHOT"]
             [com.birdseye-sw/lein-dalap "0.1.0"]]
   :profiles {:dev {:dependencies [[vimclojure/server "2.3.6"]]}}
   :main lindenmayer.server
