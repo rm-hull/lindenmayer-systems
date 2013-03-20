646998751c13286890a7d48e1ea29a974ab7ee25
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-20 20:29:05 +0000

    Bump versions

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [noir "1.3.0"]
-                 [jayq "2.1.0"]
+                 [jayq "2.3.0"]
                  [ns-tracker "0.2.1"]
                  [rm-hull/ring-gzip-middleware "0.1.5-SNAPSHOT"]
                  [rm-hull/monet "0.1.6-SNAPSHOT"]]
@@ -22,6 +22,6 @@
             [com.birdseye-sw/lein-dalap "0.1.0"]]
   :profiles {:dev {:dependencies [[vimclojure/server "2.3.6"]]}}
   :main lindenmayer.server
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.1.0"
   :warn-on-reflection true
   :description "A web-based lindenmayer-systems animator")
