ec59168812e2dc2878434203feb8e5abfbbb7d10
Richard Hull <rm_hull@yahoo.co.uk>
2016-06-10 22:47:46 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -7,14 +7,15 @@
   :dependencies [
     [org.clojure/clojure "1.8.0"]
     [org.clojure/data.json "0.2.6"]
-    [com.taoensso/timbre "4.3.1"]
+    [com.taoensso/timbre "4.4.0"]
     [compojure "1.5.0"]
-    [ring "1.4.0"]
+    [ring "1.5.0"]
     [hiccup "1.0.5"]
     [ring-logger-timbre "0.7.5"]
-    [metrics-clojure-ring "2.6.1"]
+    [metrics-clojure-ring "2.7.0"]
     [rm-hull/ring-gzip-middleware "0.1.7"]
-    [rm-hull/turtle "0.1.9-SNAPSHOT"]]
+    [rm-hull/turtle "0.1.9"]
+    [rm-hull/ring-cede "0.1.0"]]
   :scm {:url "git@github.com:rm-hull/lindenmayer-systems.git"}
   :ring {
     :handler lindenmayer.handler/app}
