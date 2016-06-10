ffa700cfa5d5d1b843e702e68889ebfd8656985f
Richard Hull <rm_hull@yahoo.co.uk>
2016-06-10 22:49:34 +0100

    Split explorer into groups

---
+++ src/lindenmayer/explorer/botany.clj
@@ -0,0 +1,38 @@
+(ns lindenmayer.explorer.botany
+  (:require
+    [lindenmayer.compiler :refer [l-system]]))
+
+(def arboria {
+
+  :shrub
+  (first
+    (drop
+      5
+      (l-system
+        "F"
+        ("^")
+        ("F=#8F[+F]F[-F][F]")
+        21.3
+        25)))
+
+  :tree
+  (first
+    (drop
+      4
+      (l-system
+        "F"
+        ("^")
+        ("F=#8FF-[#3-F+F+F]+[#9+F-F-F]")
+        22.5
+        40)))
+
+  :fractal-plant
+  (first
+    (drop
+      7
+      (l-system
+        "X"
+        ("" "^")
+        ("X=#9F-[[X]+]+F[+FX]-X" "F=FF")
+        25
+        8)))})
